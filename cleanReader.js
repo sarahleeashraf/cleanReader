998432254fbaafe05bb76cd5ad588bc48af55f09
Sarah Ashraf <sarahleeashraf@gmail.com>
2010-12-07 22:41:17 -0500

    the rest of the files

--- cleanReader.js
+++ cleanReader.js
@@ -1,9 +1,34 @@
-#qrcode {
-  position: fixed;
-  z-index: 2147483647;
-  top: 2px;
-  right: 2px;
-  background-color: white;
-  border-style: dashed;
-  border-color: black
-}
+
+$(function() {
+	$('a').each(function() {
+		var uri = this.href;
+	
+		if (uri.match(/^http:\/\/www.slate.com\/id\//))
+		{
+		
+			
+			var path = uri.split(/http:\/\/www.slate.com\/id\//)[1];
+			
+			
+			if (path.match(/^[0-9]{7}\/$/)) {
+			
+				var id = path.split(/\//)[0];
+				this.href = 'http://www.slate.com/toolbar.aspx?action=print&id=' + id;
+				$(this).css({'background-color':'#def'});
+			}
+			
+			else {
+				/*$(this).css({
+					'display':'block',
+					'border':'1px solid #00f',
+					'background-color':'#0cf',
+					'color':'#fff'		
+				});*/
+			}
+			
+		}
+	
+	});
+		
+});
+
